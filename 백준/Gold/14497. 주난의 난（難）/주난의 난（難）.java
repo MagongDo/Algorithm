a9a645547973266fe783a6eb9fdac4a84ea1a22d
도승우 <140596632+MagongDo@users.noreply.github.com>
2025-01-30 10:51:58 +0900

    [Gold IV] Title: 주난의 난(難), Time: 192 ms, Memory: 21884 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
@@ -7,44 +10,47 @@ public class Main {
     static final int[] dy = {-1, 0, 1, 0};
     static final int[] dx = {0, 1, 0, -1};
     static Queue<Integer> q = new LinkedList<>();
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        y1 = sc.nextInt() - 1;  
-        x1 = sc.nextInt() - 1; 
-        y2 = sc.nextInt() - 1;  
-        x2 = sc.nextInt() - 1;  
-        sc.nextLine();
-        
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        y1 = Integer.parseInt(st.nextToken()) - 1;
+        x1 = Integer.parseInt(st.nextToken()) - 1;
+        y2 = Integer.parseInt(st.nextToken()) - 1;
+        x2 = Integer.parseInt(st.nextToken()) - 1;
+
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextLine().toCharArray();
+            a[i] = br.readLine().toCharArray();
         }
-        
+
         q.add(1000 * y1 + x1);
         visited[y1][x1] = 1;
         int cnt = 0;
-        
+
         while (a[y2][x2] != '0') {
             cnt++;
             Queue<Integer> temp = new LinkedList<>();
-            
+
             while (!q.isEmpty()) {
                 int cur = q.poll();
                 int y = cur / 1000;
                 int x = cur % 1000;
-                
+
                 for (int i = 0; i < 4; i++) {
                     int ny = y + dy[i];
                     int nx = x + dx[i];
-                    
+
                     if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] != 0) {
                         continue;
                     }
-                    
+
                     visited[ny][nx] = cnt;
-                    
+
                     if (a[ny][nx] != '0') {
                         a[ny][nx] = '0';
                         temp.add(1000 * ny + nx);
@@ -55,8 +61,7 @@ public class Main {
             }
             q = temp;
         }
-        
+
         System.out.println(visited[y2][x2]);
-        sc.close();
     }
 }
