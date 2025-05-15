24eb091935f3782f2210c677962abdf74e0a3431
도승우 <140596632+MagongDo@users.noreply.github.com>
2025-05-15 19:56:25 +0900

    [level 2] Title: H-Index, Time: 1.00 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        
+        Arrays.sort(citations); // 오름차순
+        
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i; // 남은 논문 수
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+        return 0;
+    }
+}
+
+    
